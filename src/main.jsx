959319e4ab1cf5ed6423c2f5f2886cf508d80b64
Oybek <itpustudent@gmail.com>
2025-02-22 10:38:36 +0500

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/createx'>
+    <BrowserRouter basename='/'>
       <App />
     </BrowserRouter>
   </StrictMode>,
